f4b63a9ad43557d9604a23224bdc71cb4ef6df9c
ink0rr <tomflynnbeltsazar@gmail.com>
2022-12-29 16:45:06 +0700

    Add select prompt when no args provided

--- commands/create/create.ts
+++ commands/create/create.ts
@@ -1,11 +1,25 @@
-import { Command } from "cliffy";
+import { Command, Select } from "cliffy";
 import { block } from "./block.ts";
 import { entity } from "./entity.ts";
 import { item } from "./item.ts";
 
+const commands = {
+  block,
+  entity,
+  item,
+};
+
 export const create = new Command()
   .name("create")
-  .arguments("<option:string>")
-  .command("block", block)
-  .command("entity", entity)
-  .command("item", item);
+  .description("Create a new addon file")
+  .action(async () => {
+    const select = await Select.prompt({
+      message: "What do you want to create?",
+      options: Object.keys(commands),
+    }) as keyof typeof commands;
+    await commands[select].parse([]);
+  });
+
+for (const [name, command] of Object.entries(commands)) {
+  create.command(name, command);
+}
