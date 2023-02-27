afaa8301febb2a32bbe8a22dc05dead61c42a35c
ink0rr <tomflynnbeltsazar@gmail.com>
2023-02-27 16:11:33 +0700

    Add image merge

--- cli.ts
+++ cli.ts
@@ -1,5 +1,6 @@
 import { Command } from "cliffy";
 import { create } from "./commands/create/create.ts";
+import { merge } from "./commands/merge.ts";
 
 if (import.meta.main) {
   await new Command()
@@ -7,5 +8,6 @@ if (import.meta.main) {
     .description("A CLI utils for Bedrock")
     .arguments("<command>")
     .command("create", create)
+    .command("merge", merge)
     .parse(Deno.args);
 }
---
+++ commands/merge.ts
@@ -0,0 +1,25 @@
+import { Command } from "cliffy";
+import { Image } from "imagescript";
+
+export const merge = new Command()
+  .name("merge")
+  .description("Merges multiple images into one")
+  .arguments("[inputDir:string] [outFile:string]")
+  .action(async (_, inputDir = ".", outFile = "./output.png") => {
+    const images = [];
+    for await (const file of Deno.readDir(inputDir)) {
+      if (file.isFile && file.name.endsWith(".png")) {
+        const image = await Image.decode(await Deno.readFile(file.name));
+        if (images[0] && (image.width !== images[0].width || image.height !== images[0].height)) {
+          throw new Error("All images must be the same size");
+        }
+        images.push(image);
+      }
+    }
+    const result = new Image(images[0].width, images[0].height * images.length);
+    for (let i = 0; i < images.length; i++) {
+      const image = images[i];
+      result.composite(image, 0, i * image.height);
+    }
+    await Deno.writeFile(outFile, await result.encode());
+  });
